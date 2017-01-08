016571d86fd6cfa359a5b18bdbf8a9699059af91
Ian Moore <ianmooregis@gmail.com>
2017-01-07 19:15:03 -0600

    Initial commit

---
+++ wx-functions.js
@@ -0,0 +1,89 @@
+var wxDetails = (d) => {
+    return {
+        currentLocation: d.display_location.full,
+        currentWx: d.weather,
+        currentTemp: d.temp_f,
+        currentTempC: d.temp_c,
+        currentFeelsTemp: d.feelslike_f,
+        currentFeelsTempC: d.feelslike_c,
+        currentWind: (d.wind_mph > 0) ? `${d.wind_mph} Mph ${d.wind_dir}` : 'Calm',
+        currentWindGust: (d.wind_gust_mph > d.wind_mph) ? `gusts ${d.wind_gust_mph} Mph` : 'no gusting',
+        currentWindK: (d.wind_kph > 0) ? `${d.wind_kph} Kph ${d.wind_dir}` : 'Calm',
+        currentWindGustK: (d.wind_gust_kph > d.wind_kph) ? `gusts ${d.wind_gust_kph} Kph` : 'no gusting',
+        wxMoreDetails: d.forecast_url        
+    };
+};
+
+var getWxIcon = (wxicon, uv) => {
+  switch (wxicon) {
+    case 'chanceflurries':
+      return 'wi wi-snow-wind';
+
+    case 'chancerain':
+      return 'wi wi-rain';
+
+    case 'chancesleet':
+      return 'wi wi-sleet';
+
+    case 'chancesnow':
+      return 'wi wi-snow';
+
+    case 'chancetstorms':
+      return 'wi wi-thunderstorm';
+
+    case 'clear':
+      if (uv > 0) {
+        return 'wi wi-day-sunny';
+  
+      } else if (uv == 0 || !uv) {
+        return 'wi wi-night-clear';  
+      }
+
+    case 'cloudy':
+      return 'wi wi-cloudy';
+
+    case 'flurries':
+      return 'wi wi-snow';
+
+    case 'fog':
+      return 'wi wi-fog';
+
+    case 'hazy':
+      return 'wi wi-day-haze';
+
+    case 'mostlycloudy':
+      return 'wi wi-cloudy';
+
+    case 'mostlysunny':
+      return 'wi wi-day-sunny-overcast';
+
+    case 'overcast':
+      return 'wi wi-cloudy';
+
+    case 'partlycloudy':
+      return 'wi wi-cloudy';
+
+    case 'partlysunny':
+      return 'wi wi-day-cloudy';
+
+    case 'rain':
+      return 'wi wi-rain';
+
+    case 'sleet':
+      return 'wi wi-sleet';
+
+    case 'snow':
+      return 'wi wi-snow';
+
+    case 'sunny':
+      return 'wi wi-day-sunny';
+
+    case 'tstorms':
+      return 'wi wi-storm-showers';
+
+    case 'unknown':
+      return 'wi wi-na';
+  }
+};
+
+module.exports = {wxDetails, getWxIcon};
---
+++ wx-server.js
@@ -0,0 +1,65 @@
+const wxf = require('./wx-functions.js');
+const express = require('express');
+const hbs = require('hbs');
+const request = require('request');
+
+const port = process.env.PORT || 3000 //used for heroku
+const API_KEY = '89c9f9e4a294d471';
+
+var app = express();
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+var currentData, currentLocation, wxIcon, wxObj, errorObj;
+
+app.listen(port, () => console.log(`Server is up on port ${port}`));
+
+app.get('/', (req,res) => res.status(200).render('index.hbs'));
+
+app.get('/wx/:id', (req,res) => {
+    var id = req.params.id;
+    request({
+        url: `https://api.wunderground.com/api/${API_KEY}/conditions/q/${id}.json`,
+        json: true
+    }, (error, response, body) => {        
+        if (body.response.error) {
+            errorObj = body.response.error;
+            return res.status(404).render('E404.hbs', {
+                errorObj
+            });
+        };
+        currentData = body.current_observation;
+        wxIcon = wxf.getWxIcon(currentData.icon, currentData.UV);
+        wxObj = wxf.wxDetails(currentData);
+        return res.status(200).render('wx.hbs', {
+            wxObj,
+            wxIcon
+        });
+    });
+});
+
+app.get('/wxm/:id', (req,res) => {
+    var id = req.params.id;
+    request({
+        url: `https://api.wunderground.com/api/${API_KEY}/conditions/q/${id}.json`,
+        json: true
+    }, (error, response, body) => {        
+        if (body.response.error) {
+            errorObj = body.response.error;
+            return res.status(404).render('E404.hbs', {
+                errorObj
+            });
+        };
+        currentData = body.current_observation;
+        wxIcon = wxf.getWxIcon(currentData.icon, currentData.UV);
+        wxObj = wxf.wxDetails(currentData);
+        return res.status(200).render('wxm.hbs', {
+            wxObj,
+            wxIcon,
+        });
+    });
+});
+
+//app.use((req, res) => {
+//    res.status(404).send('error');
+//});
